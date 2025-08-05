eb1d5732306611b2fe83fcfecc7651a5e37a4a11
saifkazi25 <m.saifkazi@gmail.com>
2025-08-06 01:21:41 +0400

    -

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    // Don’t block production builds on ESLint warnings/errors
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    // Make sure the App Directory features are enabled
+    appDir: true,
+  },
 };
 
 export default nextConfig;
